df6c3f4d7efc640c2d6cad3081db73823ece59cf
Vinayak Jadhav <vjadhav1010@gmail.com>
2026-04-20 15:13:27 +0530

    comments

--- Basic-Maths/PrimeInRange.java
+++ Basic-Maths/PrimeInRange.java
@@ -11,14 +11,18 @@ class PrimeInRange{
      public static ArrayList<Integer> printPrimes(int K){
         boolean[] isPrime = new boolean[K+1];
 
+        // Fill all the numbers as true in the begining
         Arrays.fill(isPrime, true);
 
+        //remove edge cases
         if (K >= 0) isPrime[0] = false;
         if (K >= 1) isPrime[1] = false;
 
+        //starts loop from 2 to the value K
         for(int i=2; i*i<=K; i++){
             if(isPrime[i]){
                 int j = i*i;
+                //Mark non prime numbers as false
                 while(j <= K){
                     isPrime[j] = false;
                     j+=i;
